78d94132efb674684af679d0f06bf82fe3646408
Amine Ghenimi <144660090+ghenimiAmine@users.noreply.github.com>
2024-02-08 10:38:41 +0000

    running

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
 const counters = document.querySelectorAll('.counter');
+
 let scrollStarted = false;
 
 btn.addEventListener('click', navToggle);
@@ -54,6 +55,61 @@ function countUp() {
   });
 }
 
-function reset() {
-  counters.forEach((counter) => (counter.innerHTML = '0'));
+let password = document.querySelector("[name=password]")
+password.addEventListener("keyup",verifMDP)
+
+function verifMDP(){
+  if(this.value.length < 8){
+    this.classList.add("text-danger")
+    this.classlist.remove("text-valid")
+  }else{
+    this.classlist.add("text-valid")
+    this.classList.remove("text-danger")
+  }
+}
+
+function verifierFormulaire() {
+  let password = document.getElementById('password');
+  let name = document.getElementById('name');
+  let firstname = document.getElementById('firstname');
+  let email = document.getElementById('email');
+  let message = document.getElementById('message');
+  let age = document.getElementById('label');
+  let cocher = document.getElementById('formCheck-1');
+  if(password.value.length <8){
+    password.classList.add('error');
+    pwd.classList.remove('invisible');
+  }
+  else {
+    password.classList.add('good');
+    pwd.classList.add('invisible');
+  }
+  if(name.value.length <1){
+    name.classList.add('error');
+  }else {
+    name.classList.add('good');
+  }
+  if(email.value.length <1){
+    email.classList.add('error');
+  }else {
+    email.classList.add('good');
+  }
+  if(firstname.value.length <1){
+    firstname.classList.add('error');
+  }else {
+    firstname.classList.add('good');
+  }
+  if(message.value.length <1){
+    message.classList.add('error');
+  }else {
+    message.classList.add('good');
+  }
+  if(!cocher.checked){
+    age.classList.add('red');
+    age.classList.remove('vert');
+   }else{ if(cocher.checked){
+    age.classList.remove('red');
+    age.classList.add('vert');
+   }
+  }
 }
